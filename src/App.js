44e312099a410b63011c099cf1f3e0408448daab
geekelo <efueliht12@gmail.com>
2025-03-14 09:38:46 +0100

    Add json data for tests

--- src/App.js
+++ src/App.js
@@ -1,39 +1,24 @@
-
+import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Login from './Pages/Login/LoginPage';
-import Home from './Components/Home/Home';
-import CreateExam from './Components/Exam/CreateExam';
-import ExamList from './Components/ExamList/ExamList';
-import Options from './Components/OptionsPage/Options';
-import AdminHome from './Components/AdminHome/AdminHome';
-import QuestionList from './Components/QuestionList/QuestionList';
-import QuestionDetails from './Components/QuesstionDetails/QuestionDetails';
-import  { Toaster } from 'react-hot-toast';
-import { ProtectedRoute, protectedRoute } from './Hooks/IsLogin';
-import AddQuestion from './AddQuestion/question';
 
 function App() {
   return (
-    <BrowserRouter>
-    <Toaster /> 
-    <Routes>
-      <Route path="/" element={<Login/>} />
-      <Route path="/Home" element={
-      <ProtectedRoute>
-      <Home/>
-      </ProtectedRoute>
-      } >
-      <Route index element={<AdminHome/>} />
-      <Route path="create-exam" element={<CreateExam/>} />
-      <Route path="manage-exam" element={<ExamList/>} />  
-      <Route path="options/:id" element={<Options/>} />  
-      <Route path="question/:id" element={<QuestionList/>} />  
-      <Route path="questiondetails/:id" element={<QuestionDetails/>} />  
-      <Route path="create-question/:id" element={<AddQuestion/>} />  
-      </Route>
-    </Routes>
-  </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
--- src/Hooks/IsLogin.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-export const isAuthenticated = () => {
-  return localStorage.getItem("token"); // Returns token if available
-};
-
-export const ProtectedRoute = ({ children }) => {
-  return isAuthenticated() ? children : <Navigate to="/" />;
-};
