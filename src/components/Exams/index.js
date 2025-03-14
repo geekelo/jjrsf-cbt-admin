ebdc7296e86d6d9634c49f80735f76d10ea01ec7
Orji Onyinyechi <orjionyinyechi080@gmail.com>
2025-03-14 12:50:28 +0100

    Cbtadmin (#5)

--- src/components/Exams/index.js
+++ src/components/Exams/index.js
@@ -1,8 +1,21 @@
 import React, { useState } from "react";
-import { Eye } from "lucide-react";
+import { Eye, Plus } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import data from "../../data/exams.json";
 import ExamForm from "./ExamForm";
+import "../../Stylesheets/Exams.css";
+
+const formatDateTime = (dateTime) => {
+  return new Date(dateTime).toLocaleString("en-US", {
+    weekday: "short",
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: true,
+  });
+};
 
 const Exams = () => {
   const [exams, setExams] = useState(data.clacbt_exams);
@@ -16,19 +29,32 @@ const Exams = () => {
 
   return (
     <div className="exams-container">
+      <div className="exambox">
       <h2>Exams List</h2>
+      <button className="add-exam-btn" onClick={() => setShowForm(true)}>
+      <Plus /> Add New Exam 
+      </button>
+      </div>
       <ul className="exam-list">
         {exams.map((exam) => (
-          <li key={exam.id} className="exam-item">
-            <strong>{exam.name}</strong> - {exam.duration} (From {exam.start_time} to {exam.end_time})
-            <Eye className="view-icon" size={24} onClick={() => navigate(`/exam/${exam.id}`)} />
-          </li>
+         <li key={exam.id} className="exam-item">
+         <div className="exam-details">
+           <h3 className="exam-title">{exam.name}</h3>
+           <p className="exam-duration">⏳ {exam.duration} mins</p>
+           <p className="exam-time">
+             🕒 <span className="exam-time-label">From:</span> {formatDateTime(exam.start_time)}  
+             <br />
+             🕒 <span className="exam-time-label">To:</span> {formatDateTime(exam.end_time)}
+           </p>
+         </div>
+         <Eye className="view-icon" size={24} onClick={() => navigate(`/exam/${exam.id}`)} />
+       </li>
+       
+        
         ))}
       </ul>
 
-      <button className="add-exam-btn" onClick={() => setShowForm(true)}>
-        Add New Exam
-      </button>
+    
 
       {showForm && <ExamForm onClose={() => setShowForm(false)} onSubmit={addExam} />}
     </div>
